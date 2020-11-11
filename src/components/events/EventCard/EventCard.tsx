c086b11465cce934c75c52e190b3f8dbf1a48193
Ilya Popov <me.ilya.popov@gmail.com>
2020-11-12 00:40:00 +0300

    change
    localized dates with moment

--- src/components/events/EventCard/EventCard.tsx
+++ src/components/events/EventCard/EventCard.tsx
@@ -1,10 +1,14 @@
 import React, {Component} from "react";
-import * as e from "../../../models/event.model";
-import moment from 'moment';
+import {Event} from "../../../models/event.model";
+
 import './EventCard.scss';
+
+import Moment from 'react-moment';
+import 'moment/locale/ru';
+
 import iconArrowRight from '../../../assets/icons/arrow-right.svg'
 
-class EventCard extends Component<e.Event> {
+class EventCard extends Component<Event> {
     render() {
         const props = this.props;
 
@@ -16,7 +20,9 @@ class EventCard extends Component<e.Event> {
                         ? <div className="event-paid text-caption color-light">Бесплатно</div>
                         : ''}
                     <h4 className="event-title">{props.title}</h4>
-                    <div className="event-date text-caption color-light">{moment(props.when).format('lll')}</div>
+                    <div className="event-date text-caption color-light">
+                        <Moment locale="ru" date={props.when} format="lll" />
+                    </div>
                     <a className="event-link text-link color-accent" href={props.linkUrl}>
                         <span>{props.linkText}</span>
                         <img className="icon" src={iconArrowRight} alt="icon"/>
