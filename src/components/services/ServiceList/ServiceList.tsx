6a905b1d3dd76769055f10cf757c223aca5a32be
Ilya Popov <me.ilya.popov@gmail.com>
2020-11-15 19:57:44 +0300

    fix
    removed unused import

--- src/components/services/ServiceList/ServiceList.tsx
+++ src/components/services/ServiceList/ServiceList.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React from "react";
 import './ServiceList.scss';
 import {Service} from "../../../models/service.model";
 import ServiceCard from "../ServiceCard/ServiceCard";
